2892578e627f5517b0961858f9968a2925df80fb
natnael mehari <natnaeltsg27@gmail.com>
2024-05-03 20:58:58 +0000

    set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-console.log("connected!")
+function runGame() {
+
+}
+function checkAnswer() {
+
+}
+function calculateCorrectAnswer() {
+
+}
+function incrementScore () {
+
+}
+function incrementWrongAnswer() {
+
+}
+function displayAdditionQuestion() {
+
+}
+function displaySubstractQuestion() {
+
+}
+function displayMultiplyQuestion() {
+
+}
+function displayDivisionQuestion() {
+    
+}
